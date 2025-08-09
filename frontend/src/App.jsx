a4f13ab96215f710d4bdb00a36f722a3ef4a9a33
Pratiksharanade <ranadepratiksha21@gmail.com>
2025-08-10 01:13:01 +0530

    Fix case-sensitive import paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import Register from './components/Register';
+import Register from './Components/Register';
 import Login from './Components/Login';
 import AdminLogin from './Components/AdminLogin';
 import AdminDashboard from './Components/AdminDashboard';
@@ -8,12 +8,9 @@ import UserDashboard from './Components/UserDashboard';
 import Landing from './Components/Landing';
 import UserTicketHistory from './Components/UserTickethistory';
 
-
-
 function App() {
-  return (
+    return (
     <div style={{ padding: '2rem',maxWidth: '500px', margin: 'auto' }}>
-      
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/register" element={<Register />} />
