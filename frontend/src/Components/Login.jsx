e42b392d43444a7e6004adaf61e5ff0f44525318
Pratiksharanade <ranadepratiksha21@gmail.com>
2025-08-16 20:07:58 +0530

    Added Login.css and fixed import path

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -2,8 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // ✅ import
-import './Login.css';
-
+import "./Login.css";
 
 
 const Login = () => {
